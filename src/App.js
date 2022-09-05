98738135f586f88f50af578d69b0d704c96282f3
Agustin Roma <aroma@ctd.com.ar>
2022-09-05 20:32:56 -0300

    proyecto con app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import { useEffect } from 'react';
+
+import { useState, useEffect } from 'react'
+// Bootstrap CSS
+
+
 import './App.css';
 
 function App() {
-  return (
 
-    useEffect(() => {
-      //traigo la api
-      fetch("")
-      .then()
-    })
-    <div className="App">
-      
+
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("")
+    .then(res => res.json(), error => setError(error))
+    .then(json => setData(json))
+    .catch(error => setError(error))
+    .finally(() => {
+      setIsLoading(false);
+    });
+  }, [])
+    
+  return (
+    <div>
+      <h1>Marvel Search</h1>
+      <div>
+      </div>
     </div>
   );
 }
